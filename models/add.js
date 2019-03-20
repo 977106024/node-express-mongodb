1f8cdf61016721321196151adae56ceefdb259f7
bohong <977106024@qq.com>
2019-03-20 22:33:49 +0800

    冲突

--- models/add.js
+++ models/add.js
@@ -1,10 +1,11 @@
 const mongoose = require('../mongoose.js')
 const Schema = mongoose.Schema
+const Promise = require('bluebird')
 
 const textSchema = new Schema({
     text:String
 })
 
 const Text = mongoose.model('Text',textSchema)
-
+Promise.promisifyAll(Text)
 module.exports = Text
