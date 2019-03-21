ff81b954c5dca8a1e543788fcaa8a68ef9f823f9
bohong <977106024@qq.com>
2019-03-21 19:57:37 +0800

    冲突 测试

--- mongoose.js
+++ mongoose.js
@@ -1,6 +1,6 @@
 //连接数据库
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test',{useNewUrlParser:true})
+mongoose.connect('mongodb://root:Dx0w71pS7kAgF5g477oHkdt4LNMS7aOHq5FbBypf@hgmzllmfehaj.mongodb.sae.sina.com.cn:10539,vlvgmppvasvq.mongodb.sae.sina.com.cn:10539',{useNewUrlParser:true})
 // mongoose.Promise = require('bluebird')
 const con = mongoose.connection;
 con.on('error', console.error.bind(console, '连接数据库失败'));
