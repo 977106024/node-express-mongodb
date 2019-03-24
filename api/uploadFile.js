58923c77a8d071625cf1442580efc6a1afbce5b5
bohong <977106024@qq.com>
2019-03-25 01:14:53 +0800

    语音接口->成功获取前端文件->成功请求百度语音识别接口

--- api/uploadFile.js
+++ api/uploadFile.js
@@ -5,8 +5,10 @@ const fs = require('fs');
 // 文件上传
 exports.uploadFile = (req, res) => {
     console.log('录音22')
+
     //生成multiparty对象，并配置上传目标路径
-    const form = new multiparty.Form({ uploadDir: './file/recorder' });
+    const form = new multiparty.Form({ uploadDir: './api/file/recorder/' });
+
     //解析
     form.parse(req, (err, fields, files) => {
         if (err) {
@@ -16,20 +18,24 @@ exports.uploadFile = (req, res) => {
                 msg: '错误'
             })
         } else {
-            const ApiKey = '15836284';
-            const SecretKey = 'UyKarQWMrQVsTeAVjy6vvhUr';
-            // let client = new AipSpeech(0, ApiKey, SecretKey);
-            // let voice = fs.readFileSync('../assets/16k_test.pcm');
-            // let voiceBase64 = new Buffer(voice);
-            let file = files.file[0];
-            console.log(file.path)
+
+            //文件存储路径
+            let filePath = files.recorder[0].path
+
+            // 百度id Key SecretKey
+            const ApiId = '15836284'
+            const ApiKey = 'UyKarQWMrQVsTeAVjy6vvhUr';
+            const SecretKey = 'yrCNBBKFadHNLYOrrD8G7GrPYtBmLGrY';
+            let client = new AipSpeech(0, ApiKey, SecretKey);
+            let voice = fs.readFileSync(filePath);
+            let voiceBase64 = new Buffer(voice);
 
             // 识别本地语音文件
-            // client.recognize(voiceBase64, 'pcm', 16000).then(function(result) {
-            //     console.log('语音识别本地音频文件结果: ' + JSON.stringify(result));
-            // }, function(err) {
-            //     console.log(err);
-            // });
+            client.recognize(voiceBase64, 'acc', 16000).then(function(result) {
+                console.log('语音识别本地音频文件结果: ' + JSON.stringify(result));
+            }, function(err) {
+                console.log(err);
+            });
             res.json({
                 code: '2',
                 data: {
