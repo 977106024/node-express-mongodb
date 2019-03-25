e19195344497134073fc7ca35b592c4a87bc8d82
bohong <977106024@qq.com>
2019-03-25 16:41:19 +0800

    百度ai语音识别完成-待测试

--- api/uploadFile.js
+++ api/uploadFile.js
@@ -1,13 +1,16 @@
 const multiparty = require('multiparty')
 const AipSpeech = require("baidu-aip-sdk").speech;
 const fs = require('fs');
+const ffmpeg = require('../utils/ffmpge');
 
 // 文件上传
 exports.uploadFile = (req, res) => {
     console.log('录音22')
 
     //生成multiparty对象，并配置上传目标路径
-    const form = new multiparty.Form({ uploadDir: './api/file/recorder/' });
+    const form = new multiparty.Form({
+        uploadDir: './api/file/recorder/'
+    });
 
     //解析
     form.parse(req, (err, fields, files) => {
@@ -22,26 +25,37 @@ exports.uploadFile = (req, res) => {
             //文件存储路径
             let filePath = files.recorder[0].path
 
-            // 百度id Key SecretKey
-            const ApiId = '15836284'
-            const ApiKey = 'UyKarQWMrQVsTeAVjy6vvhUr';
-            const SecretKey = 'yrCNBBKFadHNLYOrrD8G7GrPYtBmLGrY';
-            let client = new AipSpeech(0, ApiKey, SecretKey);
-            let voice = fs.readFileSync(filePath);
-            let voiceBase64 = new Buffer(voice);
+            //音频格式转换
+            ffmpeg(filePath)
+            .save(`${filePath.slice(0,-4)}.wav`)
+                .on('end', function () {
+                    console.log('file has been converted succesfully');
+                    let wavPath = `${filePath.slice(0,-4)}.wav`
+                    // 百度id Key SecretKey
+                    const ApiId = '15836284'
+                    const ApiKey = 'UyKarQWMrQVsTeAVjy6vvhUr';
+                    const SecretKey = 'yrCNBBKFadHNLYOrrD8G7GrPYtBmLGrY';
+                    let client = new AipSpeech(0, ApiKey, SecretKey);
+                    let voice = fs.readFileSync(wavPath);
+                    let voiceBase64 = new Buffer(voice);
 
-            // 识别本地语音文件
-            client.recognize(voiceBase64, 'acc', 16000).then(function(result) {
-                console.log('语音识别本地音频文件结果: ' + JSON.stringify(result));
-            }, function(err) {
-                console.log(err);
-            });
-            res.json({
-                code: '2',
-                data: {
-                    msg: '?'
-                }
-            })
+                    // 识别本地语音文件
+                    client.recognize(voiceBase64, 'wav', 16000).then(function (result) {
+                        let msg = JSON.stringify(result);
+                        res.json({
+                            code: '2',
+                            data: {
+                                msg: msg
+                            }
+                        })
+
+                    }, function (err) {
+                        res.json(err)
+                    });
+                })
+                .on('error', function (err) {
+                    console.log('an error happened: ' + err.message);
+                })
         }
     })
 }
